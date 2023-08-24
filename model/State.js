81d7e325c6aecd46ccf1565a51d7f2e6680f3cc9
c0stra <ondrej.fischer@gmail.com>
2023-08-24 19:00:58 +0200

    Update State.js

--- model/State.js
+++ model/State.js
@@ -1,1 +1,17 @@
 
+export class Observable {
+
+    get() {}
+
+    observe(observer) {
+        this.observeChanges(observer)
+        observer(this.get())
+        return this
+    }
+
+    observeChanges(observer) {  }
+}
+
+export function isObservable(value) {
+    return value instanceof Observable
+}
