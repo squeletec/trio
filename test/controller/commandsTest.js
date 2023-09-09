1cf8471f4e3c1c03a7a40c65d2a791a1200d386e
c0stra <ondrej.fischer@gmail.com>
2023-09-09 22:38:00 +0200

    Update commandsTest.js

--- test/controller/commandsTest.js
+++ test/controller/commandsTest.js
@@ -1,7 +1,16 @@
-import {suite} from "../runner/test-runner.js";
+import {suite, assertEquals} from "../runner/test-runner.js";
+import {state} from "../../model/StateModel.js";
+import {increment} from "
 
 suite({
 
     name: "Controller tests"
 
+    incrementCommand() {
+        let counter = state(0)
+        let command = increment(counter)
+        assertEquals(counter.get(), 0)
+        command()
+        assertEquals(counter.get(), 1)
+    }
 })
