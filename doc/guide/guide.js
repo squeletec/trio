8a62d6ea2ee10d17621f85727d06ffce6c9b545e
c0stra <ondrej.fischer@gmail.com>
2023-10-15 20:50:33 +0200

    Update guide.js

--- doc/guide/guide.js
+++ doc/guide/guide.js
@@ -12,10 +12,10 @@ export function page(...content) {
     let index = pages.indexOf(currentPage)
     body(
         flexRow(
-            div('☰').setClass('menu'),
-            div(a('Previous').href(pages[index - 1])),
-            div('TRIO Guide: Chapter ', index + 1).auto().textCenter(),
-            div(a('Next').href(pages[index + 1]))
+            a('☰').setClass('menu'),
+            a('Previous').href(pages[index - 1]),
+            span('TRIO Guide: Chapter ', index + 1).auto().textCenter(),
+            a('Next').href(pages[index + 1])
         ).setClass('header'),
         ...content
     )
